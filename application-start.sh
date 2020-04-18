7122e1a44e1cb45d8533922a25ff2c5e87660503
Mateusz Lubanski <m.lubanskii+interview@gmail.com>
2020-04-18 15:17:55 +0200

    updating deployment scripts

--- application-start.sh
+++ application-start.sh
@@ -2,4 +2,4 @@
 
 "starging app from fat jar"
 ls -la /opt/vote-counter-*.jar
-nohup java -jar /opt/vote-counter-*.jar &
+nohup java -jar /opt/vote-counter-*.jar /dev/null 2> /dev/null < /dev/null & 
