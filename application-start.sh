3415d059582c5d765b9c3a96bc3bb5dc71858402
Mateusz Lubanski <m.lubanskii+interview@gmail.com>
2020-04-18 16:03:27 +0200

    adding validate stage

--- application-start.sh
+++ application-start.sh
@@ -2,5 +2,5 @@
 
 "starging app from fat jar"
 ls -la /opt/vote-counter-*.jar
-nohup java -jar /opt/vote-counter-*.jar /dev/null 2> /dev/null < /dev/null &
+nohup java -jar /opt/vote-counter-*.jar > /dev/null 2> /dev/null < /dev/null &
 exit 0
